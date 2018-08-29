17dab0a870767fb64789aa48aa981a8aac0f5ffc
junaidq <xcoder27@gmail.com>
2018-08-29 13:46:23 +0500

    Create MonsterList React

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,28 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+import {BrowserRouter as Router, Link, Redirect, Route} from 'react-router-dom';
+
+import Auth from './modules/Auth';
+
+import MonsterList from './components/MonsterList';
+
 class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      auth: Auth.isUserAuthenticated(),
+    }
+  }  
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Router>
+        <div className="App"> 
+        <Route exact path="/monsters" render= {()=>
+           <MonsterList /> } />
+        </div>
+      </Router>
     );
   }
 }
